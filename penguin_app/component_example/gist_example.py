b50d9ade58d1c19fcf6005c7b30a2375526fc093
JasmineLin1205 <j0938879874@gmail.com>
2023-05-06 18:18:54 +0800

    changed link

--- penguin_app/component_example/gist_example.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st 
-from streamlit_embedcode import github_gist
-
-st.title("Github Gist Example") 
-st.write("Code from Palmer's Penguin Streamlit app.")
-github_gist('https://gist.github.com/JasmineLin1205/c13a85b5fc0678666278000d46ba592d')
-
